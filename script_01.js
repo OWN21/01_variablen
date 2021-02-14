586c95b1f10fb241bc26da52055e7b3007b652c4
Oliver <dasiehstemal@gmx.de>
2021-02-14 10:38:47 +0100

    Eingabe von Kommentaren

--- script_01.js
+++ script_01.js
@@ -1,36 +1,6 @@
-/***** Variablen 01 *******/
-
-// Zeilenkommentar
-
-/**
- * Blockkommentar
- */
-
-// Ausgabe
-//console.log("hallo");
-//console.log(firstName + " " + familyName);
-
-/* Deklaration + Wertzuweisung I 
-let firstName;// Deklaration (Definition)
-firstName = "Max"; // Wertzuweisung
-let familyName = "Mütze"; // Dekl. + Wert.
-console.log(firstName);
-console.log(familyName);
-console.log(firstName + " " + familyName);
+/*** variablen 01 ****/
+/****** Variabelen 01 ******/
+/** 
+console.log("hallo");
+console.log("hi");
 */
-
-/* Deklaration + Wertzuweisung II 
-let firstName,familyName;
-firstName = prompt("Bitte Vornamen eingeben!");
-familyName = prompt("Bitte Namen eingeben!");
-console.log("Hallo, " + firstName + " " + familyName + "!");
-*/
-
-// JS ist eine untypisierte Sprache! | untyped
-let test;  // Deklaration
-test = "hi";  // String
-test = 2; // Number
-test = true; // Boolean
-
-console.log("Typ: " + typeof test);
-console.log("Inhalt: " + test);
