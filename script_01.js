aef7b64ea8e2522829eee9743c676677bdb76ca9
Oliver <dasiehstemal@gmx.de>
2021-02-14 14:06:19 +0100

    Deklaration 1 Oliver

--- script_01.js
+++ script_01.js
@@ -3,4 +3,14 @@
 /** 
 console.log("hallo");
 console.log("hi");
+
+/* Deklaration + Wertzuweisung                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+let firstName; //Deklaration 
+firstName = "Max";
+let familyName = "Mütze";
+console.log(firstName);
+console.log(familyName);
+console.log(firstName + familyName);
+console.log(firstName + " "+ familyName);
 */
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
